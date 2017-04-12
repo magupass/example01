cb9bcd62f7d1134b72af851d0c1e716298475d83
magup <magup@DESKTOP-JFARI63>
2017-04-12 22:12:44 +0900

    문자열 변수 연습 클래스 생성

---
+++ src/var/Var_string.java
@@ -0,0 +1,15 @@
+package var;
+
+public class Var_string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String first = "coding"; 
+		System.out.println(first+" "+"everybody");
+		String a, b;
+		a = "coding";
+		b = " everybody";
+		System.out.println(a+b);
+	}
+
+}
