4e43411943265ab028f88c5926f3c968ddffbaf7
magup <magup@DESKTOP-JFARI63>
2017-04-12 22:26:22 +0900

    변수의 효용 연습

---
+++ src/var/Effect_var.java
@@ -0,0 +1,19 @@
+package var;
+
+public class Effect_var {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(100 + 10);
+		System.out.println((100 + 10) / 10);
+		System.out.println(((100 + 10) / 10) - 10);
+		System.out.println((((100 + 10) / 10) - 10) * 10);
+		
+		int a = 100;
+		System.out.println(a + 10);
+		System.out.println((a+ 10) / 10);
+		System.out.println(((a + 10) / 10) - 10);
+		System.out.println((((a + 10) / 10) - 10) * 10);
+	}
+
+}
