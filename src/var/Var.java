e3664e1f0df22505997f6f852d281f6dd0bd0e52
magup <magup@DESKTOP-JFARI63>
2017-04-12 22:07:55 +0900

    변수 선언 연습

---
+++ src/var/Var.java
@@ -0,0 +1,25 @@
+package var;
+
+public class Var {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//정수
+		int a;
+		a = 1;
+		System.out.println(a+1); //2
+		 
+		a = 2;
+		System.out.println(a+1); //3
+		
+		//실수
+		double b = 1.1; 
+		System.out.println(b+1.1); // 2.2
+		 
+		b = 2.1; 
+		System.out.println(b+1.1); // 3.2
+		
+	}
+
+}
