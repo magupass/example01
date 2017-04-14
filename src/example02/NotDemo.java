356513e8ee25fd16c71bd4821657539581721096
magup <magup@DESKTOP-JFARI63>
2017-04-14 14:43:04 +0900

    NOT 논리연산

---
+++ src/example02/NotDemo.java
@@ -0,0 +1,15 @@
+package example02;
+
+public class NotDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        if (!true) {
+            System.out.println(1);
+        }
+        if (!false) {
+            System.out.println(2);
+        }
+	}
+
+}
