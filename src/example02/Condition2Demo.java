646df950f7edca253ee75e78fd8af2c2503b6c76
magup <magup@DESKTOP-JFARI63>
2017-04-14 10:11:23 +0900

    if 조건문

---
+++ src/example02/Condition2Demo.java
@@ -0,0 +1,16 @@
+package example02;
+
+public class Condition2Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        if (true) {
+            System.out.println(1);
+            System.out.println(2);
+            System.out.println(3);
+            System.out.println(4);
+        }
+        System.out.println(5);
+	}
+
+}
