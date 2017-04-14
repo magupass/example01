deda15589a2c1f8db266a2edfe36855c6e7a1174
magup <magup@DESKTOP-JFARI63>
2017-04-14 09:28:14 +0900

    명시적형변환

---
+++ src/example02/ExplicitConversion.java
@@ -0,0 +1,12 @@
+package example02;
+
+public class ExplicitConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float a = 100.0f;
+		int b = (int) 100.0F;
+		System.out.println(a+b);
+	}
+
+}
