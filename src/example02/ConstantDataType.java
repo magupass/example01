2a9912827863f40802c22ad5917eb037890cd696
magup <magup@DESKTOP-JFARI63>
2017-04-14 09:09:03 +0900

    상수 데이터 타입

---
+++ src/example02/ConstantDataType.java
@@ -0,0 +1,21 @@
+package example02;
+
+public class ConstantDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int a = 1;
+		//int a = 2.2;
+		//double a = 2.2;
+		//float a = 2.2f;
+		//int a = 2147483648;
+		//long a = 2147483648l;
+		byte a = (byte) 129;
+		short b = (short) 99999;
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+	}
+
+}
