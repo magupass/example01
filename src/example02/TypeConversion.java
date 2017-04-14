51900738c911be582b6dc3147ea28ab3df4f078b
magup <magup@DESKTOP-JFARI63>
2017-04-14 09:22:00 +0900

    형변환

---
+++ src/example02/TypeConversion.java
@@ -0,0 +1,16 @@
+package example02;
+
+public class TypeConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//double a = 3.0F;
+		//double a = 3.0;
+		int a = 3;
+		float b = 1.0F;
+		double c = a + b;
+		//System.out.println(a);
+		System.out.println(c);
+	}
+
+}
