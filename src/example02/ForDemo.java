9f4ffae4f5689742b02c3d270361a8224f9049d4
magup <magup@DESKTOP-JFARI63>
2017-04-14 14:54:26 +0900

    For 반복문

---
+++ src/example02/ForDemo.java
@@ -0,0 +1,14 @@
+package example02;
+
+public class ForDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i < 10; i++){
+			System.out.println("coding everybody : " + i);
+		}
+		
+	}
+
+}
