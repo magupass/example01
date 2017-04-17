a08179d20c02e3ec55738bd7a46b7787c4472bec
magup <magup@DESKTOP-JFARI63>
2017-04-17 22:14:01 +0900

    배열데이터 조회

---
+++ src/example02/GetDemo.java
@@ -0,0 +1,15 @@
+package example02;
+
+public class GetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] classGroup = {"ÃÖÁøÇõ","ÃÖÀ¯ºó","ÇÑ¾Æ¶÷","ÀÌ°íÀ×"};
+		System.out.println(classGroup[0]);
+		System.out.println(classGroup[1]);
+		System.out.println(classGroup[2]);
+		System.out.println(classGroup[3]);
+		System.out.println(classGroup);
+	}
+
+}
