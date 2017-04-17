391308c796b57808e461c20b21209ada6d6b3e4e
magup <magup@DESKTOP-JFARI63>
2017-04-17 22:13:07 +0900

    배열정의

---
+++ src/example02/DefineDemo.java
@@ -0,0 +1,12 @@
+package example02;
+
+public class DefineDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String[] classGroup = {"ÃÖÁøÇõ", "ÃÖÀ¯ºó", "ÇÑ¾Æ¶÷", "ÀÌ°íÀ×"	};
+		int[] classGroup = {1, 2, 3, 4	};
+		System.out.println(classGroup[1]);
+	}
+
+}
