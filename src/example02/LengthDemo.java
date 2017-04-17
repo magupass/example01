7fdd583a0047335e5b8d566337acac5b1d350364
magup <magup@DESKTOP-JFARI63>
2017-04-17 22:14:20 +0900

    배열의 크기 조회

---
+++ src/example02/LengthDemo.java
@@ -0,0 +1,18 @@
+package example02;
+
+public class LengthDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] classGroup = new String[4];
+		classGroup[0] = "√÷¡¯«ı";
+		System.out.println(classGroup.length);
+		classGroup[1] = "√÷¿Ø∫Û";
+		System.out.println(classGroup.length);
+		classGroup[2] = "«—æ∆∂˜";
+		System.out.println(classGroup.length);
+		classGroup[3] = "¿Ã∞Ì¿◊";
+		System.out.println(classGroup.length);
+	}
+
+}
