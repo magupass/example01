c0614e8fe4cf5bb200e0076b0824967877f92c8c
magup <magup@DESKTOP-JFARI63>
2017-04-17 21:59:28 +0900

    while 반복문

--- src/example02/WhileDemo.java
+++ src/example02/WhileDemo.java
@@ -4,10 +4,11 @@ public class WhileDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i = 0;
-		while (i < 10){
-			System.out.println("coding everybody : "+i*2);
-			i++;
+
+		while(true){
+			System.out.println("coding everybody");
 		}
+		
 	}
+
 }
---
+++ src/example02/WhileDemo2.java
@@ -0,0 +1,13 @@
+package example02;
+
+public class WhileDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while (i < 10){
+			System.out.println("coding everybody : "+i*2);
+			i++;
+		}
+	}
+}
