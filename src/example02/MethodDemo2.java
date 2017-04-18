68f0a02c58fd743901b73fad28dd557f113b1cff
magup <magup@DESKTOP-JFARI63>
2017-04-18 21:07:07 +0900

    메소드 없이 반복처리

---
+++ src/example02/MethodDemo2.java
@@ -0,0 +1,35 @@
+package example02;
+
+public class MethodDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while (i <10){
+			System.out.println(i);
+			i++;
+		}
+		i = 0;
+		while (i <10){
+			System.out.println(i);
+			i++;
+		}
+		i = 0;
+		while (i <10){
+			System.out.println(i);
+			i++;
+		}
+		i = 0;
+		while (i <10){
+			System.out.println(i);
+			i++;
+		}
+		i = 0;
+		while (i <10){
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+
+}
