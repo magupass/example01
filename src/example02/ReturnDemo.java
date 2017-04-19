b812947c37be6aaef6252b5961567e663599f6e1
magup <magup@DESKTOP-JFARI63>
2017-04-19 21:34:33 +0900

    메소드 리턴

---
+++ src/example02/ReturnDemo.java
@@ -0,0 +1,16 @@
+package example02;
+
+public class ReturnDemo {
+
+	public static int one(){
+		return 1;
+		//return 2;
+		//return 3;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(one());
+	}
+
+}
