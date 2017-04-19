8febe951f799924fdc6b697c25eccbc358944566
magup <magup@DESKTOP-JFARI63>
2017-04-19 21:41:41 +0900

    Strings[] args의 의미

---
+++ src/example02/InputDemo.java
@@ -0,0 +1,23 @@
+package example02;
+
+/**
+     * <B>Project Name : </B>Basic_Practice<br/>
+     * <B>Package Name : </B><br/>
+     * <B>File Name : </B>InputDemo<br/>
+     * <B>Description</B>
+     * <ul> 
+     * <li>여기에 해당 클래스에 대한 개요를 입력합니다.
+     * <li>논리적으로 내용이 분리되는 경우 li 태그를 사용하여 개행 합니다.
+     * </ul>
+     * 
+     * @author magup
+     * @since 2017. 4. 19.
+     */
+public class InputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(args.length);
+	}
+
+}
