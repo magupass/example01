95338b373d9fd5217c7f39c42696c1b1d95f6539
magup <magup@DESKTOP-JFARI63>
2017-04-20 20:26:37 +0900

    사용자 입력을 받고 결과를 반복적으로 출력

---
+++ src/example02/Scanner2Demo.java
@@ -0,0 +1,15 @@
+package example02;
+import java.util.Scanner;
+
+public class Scanner2Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNextInt()) {
+			System.out.println(sc.nextInt()*1000);
+		}
+		sc.close();
+	}
+
+}
