2cf0620c5985a859ce103f2dfd38cdf685a80468
magup <magup@DESKTOP-JFARI63>
2017-04-20 20:30:06 +0900

    메소드 호출하여 계산결과 제시

---
+++ src/example02/CalculatorDemo2.java
@@ -0,0 +1,15 @@
+package example02;
+
+public class CalculatorDemo2 {
+
+	public static void sum(int left, int right) {
+		System.out.println(left + right);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sum(11, 20);
+		sum(20, 40);
+	}
+
+}
