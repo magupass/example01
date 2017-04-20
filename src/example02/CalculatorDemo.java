909b0bb59b5bb73b76173ac184e8c2b960a147a3
magup <magup@DESKTOP-JFARI63>
2017-04-20 20:29:18 +0900

    단순계산

---
+++ src/example02/CalculatorDemo.java
@@ -0,0 +1,11 @@
+package example02;
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(10+20);
+		System.out.println(20+40);
+	}
+
+}
