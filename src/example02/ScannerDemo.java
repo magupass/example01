4b6b62a7b41e5a659c309ecba55b4a9377c41d8c
magup <magup@DESKTOP-JFARI63>
2017-04-20 20:25:52 +0900

    사용자 입력을 받고 결과 출력

---
+++ src/example02/ScannerDemo.java
@@ -0,0 +1,14 @@
+package example02;
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i*1000);
+        sc.close();
+	}
+
+}
