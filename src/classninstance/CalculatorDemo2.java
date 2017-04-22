d003162758523dc50b9b26dee1acae32846f25a9
magup <magup@DESKTOP-JFARI63>
2017-04-22 21:00:20 +0900

    main 에서 클래스 변수값 변경지정

---
+++ src/classninstance/CalculatorDemo2.java
@@ -0,0 +1,42 @@
+package classninstance;
+
+class Calculator2 {
+	static double PI = 3.14;
+	static int base = 0;
+	int left,right;
+	
+	public void setOprands (int left, int right){
+		this.left = left;
+		this.right = right;
+	}
+	
+	public void sum(){
+		System.out.println(this.left + this.right + base);
+	}
+	
+	public void avg(){
+		System.out.println((this.left + this.right + base)/2);
+	}
+}
+
+public class CalculatorDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator2 c1 = new Calculator2();
+		c1.setOprands(10, 20);
+		c1.sum();
+		
+		Calculator2 c2 = new Calculator2();
+		c2.setOprands(20, 40);
+		c2.sum();
+	
+		Calculator2.base = 10;
+		
+		c1.sum();
+		c2.sum();
+		
+		
+	}
+
+}
