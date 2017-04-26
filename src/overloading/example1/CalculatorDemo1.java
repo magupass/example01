b44039c5154aabc7b323d8181934a76d1fb31cce
magup <magup@DESKTOP-JFARI63>
2017-04-26 15:47:02 +0900

    오버로드에서 인자로 배열사용

---
+++ src/overloading/example1/CalculatorDemo1.java
@@ -0,0 +1,36 @@
+package overloading.example1;
+//오버로드에서 인자로 배열사용
+class Calculator1{
+    int[] oprands;
+     
+    public void setOprands(int[] oprands){
+        this.oprands = oprands;
+    }
+     
+    public void sum(){
+        int total = 0;
+        for(int value : this.oprands){
+            total += value;
+        }
+        System.out.println(total);
+    }
+      
+    public void avg(){
+        int total = 0;
+        for(int value : this.oprands){
+            total += value;
+        }
+        System.out.println(total/this.oprands.length);
+    }
+}
+public class CalculatorDemo1 {
+    public static void main(String[] args) {
+        Calculator1 c1 = new Calculator1();
+        c1.setOprands(new int[]{10,20});
+        c1.sum();       
+        c1.avg();
+        c1.setOprands(new int[]{10,20,30});
+        c1.sum();       
+        c1.avg();   
+    }
+}
