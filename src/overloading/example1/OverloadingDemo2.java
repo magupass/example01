f74fd06e0b8f799ab1f31886665d8ee3532a8df4
magup <magup@DESKTOP-JFARI63>
2017-04-26 15:47:44 +0900

    오버로딩 상속

---
+++ src/overloading/example1/OverloadingDemo2.java
@@ -0,0 +1,16 @@
+package overloading.example1;
+//오버로딩 상속
+public class OverloadingDemo2 extends OverloadingDemo {
+	void A (String arg1, String arg2) {System.out.println("sub class : void A (String arg1, String arg2)");}
+	void A(){System.out.println("sub class : void A()");}
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OverloadingDemo2 od = new OverloadingDemo2();
+		od.A();
+		od.A(1);
+		od.A("coding everybody");
+		od.A("coding everybody","coding everybody");
+	}
+
+}
