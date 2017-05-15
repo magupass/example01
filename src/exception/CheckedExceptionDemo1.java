722486cff3ddff8ff9e2ef56c100b42ea80c7299
magup <magup@DESKTOP-JFARI63>
2017-05-15 22:07:56 +0900

    예외의 강제1

---
+++ src/exception/CheckedExceptionDemo1.java
@@ -0,0 +1,26 @@
+package exception;
+
+import java.io.*;
+
+/**
+     * <B>Project Name : </B>Basic_Practice<br/>
+     * <B>Package Name : </B>exception<br/>
+     * <B>File Name : </B>CheckedExceptionDemo<br/>
+     * <B>Description</B>
+     * <ul> 
+     * <li>예외의 강제1.
+     * </ul>
+     * 
+     * @author magup
+     * @since 2017. 5. 10.
+     */
+public class CheckedExceptionDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//BufferedReader bReader = new BufferedReader(new FileReader("out.txt"));
+		//String input = bReader.readLine();
+		//System.out.println(input);
+	}
+
+}
