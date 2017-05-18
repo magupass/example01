c1377f2479c3f5b616e2b244a0a80ef77cef4ba4
magup <magup@DESKTOP-JFARI63>
2017-05-18 14:58:28 +0900

    상수와 enum(1/4) : 상수에 대한 복습1

---
+++ src/constant2/ConstantDemo1.java
@@ -0,0 +1,38 @@
+package constant2;
+
+/**
+     * <B>Project Name : </B>Basic_Practice<br/>
+     * <B>Package Name : </B>constant2<br/>
+     * <B>File Name : </B>ConstantDemo1<br/>
+     * <B>Description</B>
+     * <ul> 
+     * <li>상수와 enum(1/4) : 상수에 대한 복습1.
+     * </ul>
+     * 
+     * @author magup
+     * @since 2017. 5. 15.
+     */
+public class ConstantDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 1. 사과
+		 * 2. 복숭아
+		 * 3. 바나나
+		 */
+		int type = 1;
+		switch(type){
+		case 1:
+			System.out.println(57);
+			break;
+		case 2:
+			System.out.println(34);
+			break;
+		case 3:
+			System.out.println(93);
+			break;
+		}
+	}
+
+}
